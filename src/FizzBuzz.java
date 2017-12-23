215f20ecd89c3c446abcb395f4e506389549afb1
mangles <manglesfarre@gmail.com>
2017-12-23 11:00:41 +0100

    FizzBuzz exercise finished

---
+++ src/FizzBuzz.java
@@ -0,0 +1,21 @@
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+
+        for (int i=1; i<=100; i++){
+            if (i%3 == 0 && i%5 ==0){
+                System.out.print("FizzBuzz\n");
+            }
+            else if (i%3 == 0){
+                System.out.print("Fizz\n");
+            }
+            else if (i%5 == 0){
+                System.out.print("Buzz\n");
+            }
+            else {
+
+                System.out.println(i);
+            }
+        }
+    }
+}
